3dbde815d475352f174a2e0a37e6105f9ac2aea4
rushmi0 <120770468+rushmi0@users.noreply.github.com>
2023-02-15 21:47:25 +0700

    Delete test.java

--- HDkey/src/test.java
+++
@@ -1,9 +0,0 @@
-import java.util.Base64;
-
-public class test {
-    public static void main(String[] args) {
-        byte[] input = new byte[] {1, 2, 3, 4, 5};
-        String output = Base64.getEncoder().encodeToString(input);
-        System.out.println("Base64 string representation: " + output);
-    }
-}
